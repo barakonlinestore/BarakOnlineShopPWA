3c19f4d6b68d7accb600a15a0e7c73e13f7ae872
Zakaria talukdar <zakariatalukdar123@gmail.com>
2025-07-01 21:10:17 +0000

    So after deploy will it be working like pwa or anything more that I need

--- public/sw.js
+++ public/sw.js
@@ -1,46 +1,38 @@
-
 const CACHE_NAME = 'barak-shop-cache-v1';
 const urlsToCache = [
   '/',
   '/manifest.webmanifest',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
+      .then((cache) => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', event => {
-  // We only want to cache GET requests.
-  if (event.request.method !== 'GET') {
-    return;
-  }
-  
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request).catch(() => {
-      // If the fetch fails (e.g., offline), we try to serve from cache.
-      return caches.match(event.request).then(response => {
-        // If we have a cached response, serve it.
-        // Otherwise, the app's frontend will show the offline message.
-        return response;
+      return caches.match(event.request).then((response) => {
+        if (response) {
+          return response;
+        }
       });
     })
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
